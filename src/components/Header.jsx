a7ee57e28f8d98a5883d315fd28cb4f6a8b2edfd
AsifAnsar-i <91591849+AsifAnsar-i@users.noreply.github.com>
2024-02-01 19:38:43 +0530

    commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,10 +6,12 @@ const Header = () => {
   const { cart } = useContext(CartContext);
 
   return (
-    <div className="w-[1440px] sticky top-0 bg-slate-200 self-center mx-auto items-center p-3 rounded-md">
+    <div className="w-full bg-slate-200 p-3 sm:sticky sm:top-0 sm:self-center sm:mx-auto sm:items-center sm:rounded-sm">
       <div className="flex items-center justify-between p-3 mx-auto">
         {/* Logo */}
-        <div className="font-bold text-2xl text-black uppercase ">shopping Store</div>
+        <div className="font-bold text-xl text-black uppercase">
+          Shopping Store
+        </div>
 
         {/* Navigation */}
         <ul className="flex justify-end gap-4">
